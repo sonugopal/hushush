b777ba440ad04061458290028a398f5a318a1a08
sonu <sonugopal41@gmail.com>
2018-02-20 22:15:07 +0530

    videos added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,28 @@ $(document).ready(function() {
     $('.drawer').drawer();
     $(".drawer-menu-item").click(function () {
         $('.drawer').drawer('close');
+    })
+
+
+    $('#video_modal1').on('hide.bs.modal', function (e) {
+        $('video').get(0).pause()
+    });
+
+    $("#play1").click(function () {
+        $("#couple").show();
+    })
+    $("#close1").click(function () {
+        $("#couple").hide();
+        $('#couple video').get(0).pause()
+
+
+    })
+    $("#play2").click(function () {
+        $("#family").show();
+    })
+    $("#close2").click(function () {
+        $("#family").hide();
+        $('#couple video').get(0).pause()
 
 
     })
